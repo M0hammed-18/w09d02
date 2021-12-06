be17989e946214f92b5e5140f2a484a165a7189e
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-12-06 19:26:27 +0300

    FuncationSingInInLoginReducersDone

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -2,3 +2,23 @@ const instialState = {
     user: null,
     token: "",
   };
+
+  const signIn = (state = instialState, action) => {
+    const { type, payload } = action;
+  
+    switch (type) {
+      case "LOGIN":
+        const { user, token } = payload;
+        localStorage.setItem("token", token);
+        return { user, token };
+  
+      case "LOGOUT":
+        localStorage.clear();
+        return payload;
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default signIn;
