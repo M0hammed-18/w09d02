9d239036f7f0ef029cceeab832f5264973ece913
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-12-06 21:30:30 +0300

    IndexDone

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./App";
+import store from "./reducers";
+import {Provider} from "react-redux"
 ReactDOM.render(
+  <Provider store={store}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
